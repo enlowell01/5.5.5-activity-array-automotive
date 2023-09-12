7456148e82f7607f252b884d1b2a39ae46b84bab
Evan <evanlowell01@gmail.com>
2023-09-12 16:46:52 -0400

    fixed

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,5 +1,5 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle").Vehicle
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
